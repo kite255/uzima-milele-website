0066e854ba04e3ad8f64f686f7233915879f7c9f
Kitenken Lucas <kitenken@gmail.com>
2026-09-13 02:18:17 +0300

    feat: automate devotion campaign scheduling in filament

--- app/Filament/Resources/DevotionResource/Pages/CreateDevotion.php
+++ app/Filament/Resources/DevotionResource/Pages/CreateDevotion.php
@@ -3,10 +3,23 @@
 namespace App\Filament\Resources\DevotionResource\Pages;
 
 use App\Filament\Resources\DevotionResource;
-use Filament\Actions;
+use App\Models\Devotion;
+use App\Services\DevotionEmailAutomationService;
 use Filament\Resources\Pages\CreateRecord;
 
 class CreateDevotion extends CreateRecord
 {
     protected static string $resource = DevotionResource::class;
+
+    protected function afterCreate(): void
+    {
+        /** @var Devotion $devotion */
+        $devotion = $this->record;
+
+        app(
+            DevotionEmailAutomationService::class
+        )->sync(
+            $devotion
+        );
+    }
 }
