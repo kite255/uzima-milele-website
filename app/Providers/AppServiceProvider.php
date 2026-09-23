a1e45ee693faa64a0de4d2e7e1e7678fcb36f6d2
kite255 <160236713+kite255@users.noreply.github.com>
2026-09-24 00:54:19 +0300

    feat: register campaign click tracking route

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use App\Http\Controllers\EmailCampaignClickController;
+use Illuminate\Support\Facades\Route;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +21,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        Route::get(
+            '/email/click/{token}',
+            EmailCampaignClickController::class
+        )->name('email-campaigns.click');
     }
 }
